0e35ae91381100f16cd080ac34c0b8a8eeec2fd3
feihb123 <feihb123@vip.qq.com>
2019-07-22 16:09:02 +0800

    update

---
+++ src/components/consumer/diy/index.vue
@@ -0,0 +1,24 @@
+<!-- 个性定制 -->
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'',
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  methods: {}
+}
+
+</script>
+
+<style lang='scss' scoped>
+
+</style>
